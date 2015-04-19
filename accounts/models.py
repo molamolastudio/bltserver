ff86d40d571ab46daa9fc609bbff46a8ba3c2f6a
Andhieka Putra <andhieka.putra@gmail.com>
2015-04-19 17:21:56 +0800

    Remove unique constraint from username

--- accounts/models.py
+++ accounts/models.py
@@ -6,6 +6,11 @@ from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+
+# Remove unique contraint from username
+User._meta.get_field('username')._unique = False
+
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
